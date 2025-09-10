2e03ee99623d2db508a392e2afb8f885c3d7ec5c
Booboo <jennifer910103@gmail.com>
2025-09-10 17:24:07 +0800

    added app.get, port, and listener

--- myApp.js
+++ myApp.js
@@ -1,10 +1,17 @@
-let express = require('express');
-let app = express();
-console.log("Hello World");
+let express = require('express');   // import express module
+let app = express();    // name of this app
+console.log("Hello World"); // assignment 1/12
 
+let port = 3000;    // port number
 
+// assignment 2/12
+app.get("/", (req, res) => {
+    res.send("Hello Express");
+});
 
-
+let listener = app.listen(port,() => {
+    console.log('Your app is listening on port ' + listener.address().port);
+})
 
 
 
